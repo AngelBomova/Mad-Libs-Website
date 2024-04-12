45fb1f3d8af2ab2da253eaebf4c6ebe868ade66b
224abomova <224abomova@frhsd.com>
2024-04-12 09:28:40 -0400

    rough draft done

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function playMadLibs() {
     var username = document.getElementById("username").value;
-    var player = document.getElementById("nbaPlayer").value;
-    var team = document.getElementById("nbaTeam").value;
+    var nbaPlayer = document.getElementById("nbaPlayer").value;
+    var nbaTeam = document.getElementById("nbaTeam").value;
+    var number = document.getElementById("numGames").value;
     var videoGame = document.getElementById("videoGame").value;
-    var number = document.getElementById("num1").value;
     var place = document.getElementById("place").value;
     var year = document.getElementById("year").value;
   
-    var story = `
-        <b>Your MadLibs Story: Two worlds collide!</b><br>
-        ${player} from the NBA and ${team} rescue the cast of ${videoGame} in an epic battle of dunks, three-pointers, and alley-oops! The evil team owner ${username} transports our two heroes to ${place} in the year ${year} to challenge ${username}'s evil team of ${number} players! Who will emerge victorious? Watch and find out!<br><br>
-        Coming Fall ${year + 1}!
-    `;
+    var story = `${username} and ${nbaPlayer} from the ${nbaTeam} enjoy some ${number} while playing some ${videoGame} in ${place}. The ${nbaTeam} will win the ${year} NBA Finals.`;
   
     document.getElementById("showStory").innerHTML = story;
 }
